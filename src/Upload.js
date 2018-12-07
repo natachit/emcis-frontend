77fb90cf5813688bd182fa7543b0b78edbbcc9f4
natachit <natachit@hotmail.com>
2018-12-07 12:29:49 +0700

    change spinner loading

--- src/Upload.js
+++ src/Upload.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { css } from 'react-emotion';
-import Loader from 'react-spinners/PulseLoader';
+import Loader from 'react-spinners/SyncLoader';
 
 import './App.css'
 
