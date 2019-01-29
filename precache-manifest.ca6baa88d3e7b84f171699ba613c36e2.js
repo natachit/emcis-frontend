0d259046270a90bfeae282a0388ab63ed9d1a65a
natachit <natachit@hotmail.com>
2019-01-29 14:20:46 +0700

    Updates

---
+++ precache-manifest.ca6baa88d3e7b84f171699ba613c36e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8500612e6496ce3c4a80",
+    "url": "/emcis-frontend/static/js/runtime~main.8500612e.js"
+  },
+  {
+    "revision": "b9a0df7cff8c4831f4ce",
+    "url": "/emcis-frontend/static/js/main.b9a0df7c.chunk.js"
+  },
+  {
+    "revision": "b83b21f7965b48eb72b0",
+    "url": "/emcis-frontend/static/js/1.b83b21f7.chunk.js"
+  },
+  {
+    "revision": "b9a0df7cff8c4831f4ce",
+    "url": "/emcis-frontend/static/css/main.ee3d4ed2.chunk.css"
+  },
+  {
+    "revision": "baabc9f5360a349b417751b025bd173f",
+    "url": "/emcis-frontend/index.html"
+  }
+];
